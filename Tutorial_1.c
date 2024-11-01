bcdbce7f7c968d5dda9b39a45fea7680626b461f
Dev Kumar <devkr074@gmail.com>
2024-11-01 10:43:23 -0700

    Printing Messages and Comments in C

---
+++ Tutorial_1.c
@@ -0,0 +1,15 @@
+// Printing Messages and Comments in C
+#include <stdio.h>
+int main()
+{
+    // This is Single Line Comment
+    /*
+    This
+    is
+    Multi-Line
+    Comment
+    */
+    printf("I like pizza!\n");
+    printf("It's delicious\n");
+    return 0;
+}
