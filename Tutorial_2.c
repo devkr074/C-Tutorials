3e0b443d6dcba69c3433a2f5986a4ee176fc8b82
Dev Kumar <devkr074@gmail.com>
2024-11-01 10:45:02 -0700

    Escape Sequences in C

---
+++ Tutorial_2.c
@@ -0,0 +1,10 @@
+// Escape Sequences in C
+#include <stdio.h>
+int main()
+{
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\"I Like Pizza\" - Abraham Lincoln\n");
+    printf("\'I Like Pizza\' - Abraham Lincoln\n");
+    printf("\\I Like Pizza\\ - Abraham Lincoln");
+    return 0;
+}
